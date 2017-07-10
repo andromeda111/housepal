31da72cf802585794518a215ab7b4b9f55128ae1
John <jmseymour111@gmail.com>
2017-07-10 10:20:33 -0700

    added update and delete routes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,10 +13,12 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, $http) {
 
+  const api = 'http://localhost:9000/list'
+
   $scope.$on('$ionicView.enter', function(e) {
     console.log('on init');
     $scope.stuff = []
-    $http.get('http://localhost:9000/list').then(result => {
+    $http.get(`${api}`).then(result => {
       console.log(result);
       $scope.stuff = result.data
     })
@@ -27,6 +29,32 @@ function ($scope, $stateParams, $http) {
     $scope.activeClass = id;
     console.log(id);
   };
+
+  $scope.updateItem = function (item) {
+    console.log(item);
+    const id = item.id
+    // const user = item['user_name']
+
+    $http.put(`${api}/${id}`, item).then(res => {
+      $http.get(`${api}`).then(result => {
+        console.log(result);
+        $scope.stuff = result.data
+      })
+    })
+  }
+
+  $scope.deleteItem = function (item) {
+    const id = item.id
+    $http.delete(`${api}/${id}`).then(res => {
+      console.log('DELETED, BACK AT STATE: RES: ');
+      console.log(res);
+      $http.get(`${api}`).then(result => {
+        console.log(result);
+        $scope.stuff = result.data
+      })
+    })
+  }
+  // End Controller
 }])
 
 .controller('messageBoardCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
