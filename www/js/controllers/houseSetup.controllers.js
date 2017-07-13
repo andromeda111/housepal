0e1dd66b0e912066bbb6f3a8b83f77165efb7d28
John <jmseymour111@gmail.com>
2017-07-12 21:27:50 -0700

    house join and create routes work

--- www/js/controllers/houseSetup.controllers.js
+++ www/js/controllers/houseSetup.controllers.js
@@ -7,22 +7,33 @@ angular.module('app.houseSetup.controllers', [])
         $scope.hideIntro = true
       });
 
-      $scope.step1 = function() {
+      $scope.joinButton = function() {
         $scope.showJoinContainer = true
         $scope.hideIntro = false
       }
 
-      $scope.joinFormSubmit = function (joinFormCode) {
-        console.log(joinFormCode);
-        $http.post('http://localhost:9000/houses/join', {joinFormCode}).then(result => {
-          console.log('returned from post, success');
-          console.log(result.data.newToken);
+      $scope.createButton = function() {
+        $scope.showCreateContainer = true
+        $scope.hideIntro = false
+      }
+
+      $scope.joinFormSubmit = function (joinFormData) {
+        $http.post('http://localhost:9000/houses/join', joinFormData).then(result => {
           AuthService.storeUserCredentials(result.data.newToken);
           $state.go('tabsController.messageBoard')
         }).catch(err => {
           console.log(err);
-          console.log(err.status, err.data.msg);
-          $scope.joinError = {text: 'try again, house doesnt exist', err: err.data.msg}
+          console.log(err.status, err.data.msg, err.data.err);
+          $scope.joinError = {text: 'try again, house doesnt exist', msg: err.data.msg, err: err.data.err.detail}
+        })
+      }
+
+      $scope.createFormSubmit = function (createFormData) {
+        $http.post('http://localhost:9000/houses/create', createFormData).then(result => {
+          AuthService.storeUserCredentials(result.data.newToken);
+          $state.go('tabsController.messageBoard')
+        }).catch(err => {
+          $scope.createError = {text: 'try again, house already exists', msg: err.data.msg, err: err.data.err.detail}
         })
       }
 
