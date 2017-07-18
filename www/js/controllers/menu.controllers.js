723a72a7c4bb4ad2e5676eeb946f57b5b9cc240a
John <jmseymour111@gmail.com>
2017-07-18 12:14:03 -0700

    shifted test buttons to side menu, logout button now works and closes menu

--- www/js/controllers/menu.controllers.js
+++ www/js/controllers/menu.controllers.js
@@ -1,5 +1,27 @@
 angular.module('app.menu.controllers', [])
 
-  .controller('menuCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
+  .controller('menuCtrl', ['$scope', '$state', '$stateParams', '$http', 'AuthService', '$ionicSideMenuDelegate', function($scope, $state, $stateParams, $http, AuthService, $ionicSideMenuDelegate) {
+
+    $scope.destroySession = function() {
+      AuthService.logout();
+    };
+
+    $scope.getInfo = function() {
+      $http.get('http://localhost:9000/memberinfo').then(function(result) {
+        $scope.memberinfo = result.data;
+      });
+    };
+
+    $scope.deltest = function() {
+      $http.get('http://localhost:9000/deleteHouse').then(function(result) {
+        $scope.memberinfo = 'deleted house 1';
+      });
+    };
+
+    $scope.logout = function() {
+      AuthService.logout();
+      $ionicSideMenuDelegate.toggleLeft(false);
+      $state.go('login');
+    };
 
   }])
